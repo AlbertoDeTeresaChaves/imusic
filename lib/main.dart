d84c44e5a91f4f3c2fa4d0233c5fb0c87de0e4e1
Beto <150013418+AlbertoDeTeresaChaves@users.noreply.github.com>
2024-11-27 14:16:34 +0100

    Agregando la utilizacion de los paquetes al proyecto

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:imusic/firebase_options.dart';
 import 'package:imusic/pages/LoginPage.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
